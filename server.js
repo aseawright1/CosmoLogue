bab6f8327cf5fb22eb4ee3d28d1f5c110fb3bcd3
Alek <56727628+aseawright1@users.noreply.github.com>
2020-05-10 23:02:09 -0500

    reorganizing for heroku

--- server.js
+++ server.js
@@ -11,11 +11,17 @@ app.use(express.json());
 
 // static assets (heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static("client/build"));
 }
 
 app.use(routes);
-mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password1@ds043190.mlab.com:43190/heroku_4l18zspb");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password1@ds061200.mlab.com:61200/heroku_rp1m3s3w");
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(port, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${port}!`);
